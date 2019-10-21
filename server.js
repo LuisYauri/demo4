861e9e4e7524bfb09ee50c6c7d3800f33154c2b2
Luis Yauri <david.yauri147@gmail.com>
2019-10-21 12:23:59 -0500

    ga

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname+'/dist/dem'));
-app.get('*',function(req,res){
+app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/dist/dem/index.html'));
 });
 
